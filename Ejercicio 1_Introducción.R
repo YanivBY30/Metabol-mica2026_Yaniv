35e9f8442e1a0ddb62f39b50dfcf7325ee48e776
Yaniv Bar Yosef <baryosefy@gmail.com>
2026-01-23 15:39:52 -0600

    Add files via upload

---
+++ Ejercicio 1_Introducción.R
@@ -0,0 +1,25 @@
+2+2
+
+#Clase de metabolómica
+
+2^(-4)
+
+Perros <- 15
+
+Ovejas <- 17
+
+Perros+Ovejas
+
+
+CHO <- 535000
+VERO <- 1350000
+TOTAL <- CHO+VERO
+"----------"
+Nombre<-c("Manuel","Jesus","Juana","Yaniv")
+Altura<-c(1.77,1.70,1.60,1.79)
+Edad<- c(42,21,22,23)
+Mascota<-c("si","no","si","si")
+
+Tabla <- data.frame(Nombre,Altura,Edad,Mascota)
+Tabla
+View(Tabla)
