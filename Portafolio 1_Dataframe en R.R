d5d1e72421dd0ae867b41d5e10dc176cc479c0bc
Yaniv Bar Yosef <baryosefy@gmail.com>
2026-02-11 22:53:03 -0600

    Add files via upload

---
+++ Portafolio 1_Dataframe en R.R
@@ -0,0 +1,19 @@
+Pelicula <-c("Anora","The Brutalist","Flow","Emilia Pérez","Wicked","Dune: Part Two","I’m Still Here")
+
+`Premios Oscar 2025` <-c(
+  "Mejor Película, Mejor Director, Mejor Actriz, Mejor Guion Original, Mejor Montaje",
+  "Mejor Actor, Mejor Fotografía, Mejor Banda Sonora",
+  "Mejor Película de Animación",
+  "Mejor Actriz de Reparto, Mejor Canción Original",
+  "Mejor Diseño de Vestuario, Diseño de Producción",
+  "Mejor Sonido, Mejores Efectos Visuales",
+  "Mejor Película Internacional"
+)
+
+`Mes de estreno` <- c("Enero","Diciembre","Diciembre","Mayo","Noviembre","Marzo","Febrero")
+
+`Recaudación mundial (estimada)` <- c(41000000,41400000,728000000,714000000,20000000,29900000,30000000)
+
+Tabla <- data.frame(Pelicula, `Premios Oscar 2025`, `Mes de estreno`, `Recaudación mundial (estimada)`)
+
+View(Tabla)
